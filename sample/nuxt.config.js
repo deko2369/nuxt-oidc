eec1292f77d286d522f128d91da39e315399f8ff
deko2369 <keinissy@gmail.com>
2021-02-05 01:18:46 +0900

    added sample project

---
+++ sample/nuxt.config.js
@@ -0,0 +1,22 @@
+export default {
+  components: true,
+  modules: ['../lib/module'],
+  oidc: {
+    issuer: process.env.OIDC_ISSUER,
+    clientId: process.env.OIDC_CLIENT_ID,
+    clientSecret: process.env.OIDC_CLIENT_SECRET,
+    callbackUrl: 'http://localhost:3000/oidc/callback',
+    scope: [
+      'email',
+      'profile',
+      'address'
+    ],
+    // express-session configuration
+    session: {
+      secret: process.env.OIDC_SESSION_SECRET,
+      cookie: {},
+      resave: false,
+      saveUninitialized: false
+    }
+  },
+}
---
+++ sample/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>isLoggedIn: {{ $oidc.isLoggedIn }}</p>
+    <p>user: {{ $oidc.user }}</p>
+    <button v-if="!$oidc.isLoggedIn" @click="$oidc.login()">
+      Login
+    </button>
+    <button v-else @click="$oidc.logout()">
+      Logout
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['oidc']
+}
+</script>
